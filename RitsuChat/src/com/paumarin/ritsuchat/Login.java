8904a8d9b4a1fe1e8458d61e879a25445a84991e
Pau-Marin <ritsu95@icloud.com>
2023-01-23 20:16:06 +0100

    Episode 3

--- RitsuChat/src/com/paumarin/ritsuchat/Login.java
+++ RitsuChat/src/com/paumarin/ritsuchat/Login.java
@@ -2,37 +2,81 @@ package com.paumarin.ritsuchat;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 	private JTextField txtName;
+	private JTextField txtAddress;
+	private JLabel lblAddress;
+	private JLabel lblPort;
+	private JTextField txtPort;
+	private JLabel lblAddressDesc;
+	private JLabel lblPortDesc;
 
 	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300,380);
+		setSize(300, 380);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		txtName = new JTextField();
-		txtName.setBounds(67, 50, 165, 28);
+		txtName.setBounds(59, 50, 165, 28);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Name:");
-		lblNewLabel.setBounds(127, 34, 45, 16);
-		contentPane.add(lblNewLabel);
+
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(119, 34, 45, 16);
+		contentPane.add(lblName);
+
+		txtAddress = new JTextField();
+		txtAddress.setBounds(59, 116, 165, 28);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+
+		lblAddress = new JLabel("IP Address:");
+		lblAddress.setBounds(103, 96, 77, 16);
+		contentPane.add(lblAddress);
+
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(59, 191, 165, 28);
+		contentPane.add(txtPort);
+
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(125, 171, 34, 16);
+		contentPane.add(lblPort);
+
+		lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
+		lblAddressDesc.setBounds(86, 142, 112, 16);
+		contentPane.add(lblAddressDesc);
+
+		lblPortDesc = new JLabel("(eg. 8192)");
+		lblPortDesc.setBounds(108, 218, 68, 16);
+		contentPane.add(lblPortDesc);
+
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(83, 287, 117, 29);
+		contentPane.add(btnLogin);
 	}
 
 	public static void main(String[] args) {
