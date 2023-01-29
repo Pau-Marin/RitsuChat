51bb3a426a5a77a34dfb85c1ffbee3764db3467b
Pau-Marin <ritsu95@icloud.com>
2023-01-29 21:43:08 +0100

    Episode 15

--- RitsuChat/src/com/paumarin/ritsuchat/server/Server.java
+++ RitsuChat/src/com/paumarin/ritsuchat/server/Server.java
@@ -1,11 +1,52 @@
 package com.paumarin.ritsuchat.server;
 
-public class Server {
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server implements Runnable {
+
+	private DatagramSocket socket;
+	private Thread run, manage, send, receive;
 
 	private int port;
+	private boolean running = false;
 
 	public Server(int port) {
 		this.port = port;
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		run = new Thread(this, "Server");
+	}
+
+	public void run() {
+		running = true;
+		manageClients();
+		receive();
+	}
+
+	private void manageClients() {
+		manage = new Thread("Manage") {
+			public void run() {
+				while (running) {
+
+				}
+			}
+		};
+		manage.start();
+	}
+
+	private void receive() {
+		receive = new Thread("Receive") {
+			public void run() {
+				while (running) {
+
+				}
+			}
+		};
+		receive.start();
 	}
 
 }
